2efb37b854bf8197779293ab58f6854c17bf0ae4
NIKOLAY VIKTOROVICH <145424511+NikolayViktorovich@users.noreply.github.com>
2026-01-20 03:10:27 +0500

    happydom-upd

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: 'happy-dom',
     setupFiles: './src/test/setup.ts',
     exclude: ['**/node_modules/**', '**/dist/**', '**/e2e/**'],
     coverage: {
